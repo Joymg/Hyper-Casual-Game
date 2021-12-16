cee9357578c5965011f50c7ad17c7d02bf1e96b3
Joymg <josemg98and@gmail.com>
2021-12-16 14:07:26 +0100

    Adding support for Cubic Bezier curves, and improving representation in edit mode

--- Assets/Scripts/Bezier.cs
+++ Assets/Scripts/Bezier.cs
@@ -2,22 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bezier 
+public class Bezier
 {
-    public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2,float t)
+    /// <summary>
+    /// Quadratic Bezier curve 
+    /// </summary>
+    public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, float t)
     {
-        //Lerping from 1st to 2nd and form 2nd to 3rd
-        //return Vector3.Lerp(Vector3.Lerp(p0, p1, t), Vector3.Lerp(p1, p2, t), t);
         t = Mathf.Clamp01(t);
         float oneMinusT = 1f - t;
-        return oneMinusT * oneMinusT * p0 + 2 * oneMinusT * t * p1 + t * t * p2;
+        return oneMinusT * oneMinusT * p0 +
+            2f * oneMinusT * t * p1 +
+            t * t * p2;
     }
 
     /// <summary>
-    /// Calculates lines tangent to the cure, which can be interpreted as the speed we move along the curve
+    /// Cubic Bezier curve 
+    /// </summary>
+    public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+    {
+        t = Mathf.Clamp01(t);
+        float oneMinusT = 1f - t;
+        return oneMinusT * oneMinusT * oneMinusT * p0 +
+            3f * oneMinusT * oneMinusT * t * p1 +
+            3f * oneMinusT * t * t * p2 +
+            t * t * t * p3;
+    }
+
+
+    /// <summary>
+    /// Calculates lines tangent to the quadratic curve, which can be interpreted as the speed we move along the curve
     /// </summary>
     public static Vector3 GetFirstDerivative(Vector3 p0, Vector3 p1, Vector3 p2, float t)
     {
-        return 2 * (1f - t) * (p1 - p0) + 2 * t *(p2 - p1);
+        return 2 * (1f - t) * (p1 - p0) + 2 * t * (p2 - p1);
+    }
+
+    //Calculates lines tangent to the cubic curve, which can be interpreted as the speed we move along the curve
+    public static Vector3 GetFirstDerivative(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+    {
+        t = Mathf.Clamp01(t);
+        float oneMinusT = 1f - t;
+        return
+            3f * oneMinusT * oneMinusT * (p1 - p0) +
+            6f * oneMinusT * t * (p2 - p1) +
+            3f * t * t * (p3 - p2);
     }
 }
